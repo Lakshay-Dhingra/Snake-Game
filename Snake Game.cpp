2550da31bd9d1ac03d37dfd0978cab53b71e39af
Lakshay <lakshaydhingra32969@gmail.com>
2020-01-31 13:02:53 +0530

    v5

--- Snake Game.cpp
+++ Snake Game.cpp
@@ -2,16 +2,21 @@
 #include<conio.h>
 #include<cstdlib>
 #include<ctime>
-#include "LinkedList.h"
+#include "LinkedList.h"	//My user-defined header file for linked list
 #include <windows.h>
 using namespace std;
+
+// NOTE:
+// 1. I've assumed x-axis as vertically downward and y-axis horizontally toward right
+// 2. Run this code along with command -std=c++11
+
 
 class Food
 {
 	public:
 	int foodx;
 	int foody;
-	void genFood(int l,int h)
+	void genFood(int l,int h)	//To Generate Random Food For Snake To Eat
 	{
 		srand(time(0));
 		foodx=(rand()%(l-2))+1;
@@ -26,7 +31,7 @@ class Snake
 	LL* mysnake;
 	
 	public:
-	Snake(int l,int x,int y)
+	Snake(int l,int x,int y)	//Constructor that creates a linked list for snake with initial lenght l at postion (x,y)
 	{
 		mysnake=new LL();
 		for(int i=0;i<l;i++)
@@ -36,33 +41,33 @@ class Snake
 		length=l;
 	}
 	
-	int getLength()
+	int getLength() //Returns Length of Snake
 	{
 		return length;
 	}
 	
-	void eatFood(int x,int y)
+	void eatFood(int x,int y)	//Increases snakes length at head when it eats food
 	{
 		length+=1;
 		mysnake->insert(x,y);
 	}
 	
-	void move(int x,int y)
+	void move(int x,int y)	//To move the snake
 	{
 		mysnake->insert(x,y);
 		mysnake->del();
 	}
 	
-	int* showSnake()
+	int* showSnake()	//Returns an array containing x and y corrdinates of snake
 	{
 		return mysnake->show(length);
 	}
 	
-	int getHeadX()
+	int getHeadX()		//Returns x coordinate of snake's head
 	{
 		return mysnake->head->getx();
 	}
-	int getHeadY()
+	int getHeadY()		//Returns y coordinate of snake's head
 	{
 		return mysnake->head->gety();
 	}
@@ -70,46 +75,39 @@ class Snake
 
 class Window
 {
-	int length;
-	int hieght;
-	int snk_direction;
-	Snake* snk;
-	Food* fd;
-	int* arr;
+	int length;	//vertical length
+	int hieght;	//horizontal hieght
+	int snk_direction;	//Current direction of movement of snake
+	Snake* snk;	//Stores Snake object which is to be shown in the window
+	Food* fd;	//Stores Food object which is to be shown in the window
+	int* arr;	//Snake's x and y coordinates in array
+	int score;
 	
 	public:
-	Window(int l,int h,int posx,int posy)
+	Window(int l,int h,int posx,int posy)	//Constructor
 	{
-		
 		length=l;
 		hieght=h;
 		snk_direction=80;
 		snk=new Snake(4,posx-3,posy);
 		fd=new Food();
-//		arr=snk->showSnake();
-		
-//		rungame();
-//		For Debugging:
-//		for(int i=0;i<2*(sk.getLength());i++)
-//		{
-//			cout<<arr[i]<<" ";
-//		}
+		score=0;
 	}
 	
-	void rungame()
+	void rungame()	//Runs the actual game 
 	{
 		char ch;
-		fd->genFood(length,hieght);
+		fd->genFood(length,hieght);		//To generate new food for the first time
 		while(true)
 		{
-			if(kbhit())
+			if(kbhit())				//Takes input from the user and performs required operation
 			{
 				ch = _getch();
-				if(int(ch)==27)
+				if(int(ch)==27)		//27--> ascii for ESC key
 					break;
-				else if(int(ch)==-32||int(ch)==0)
+				else if(int(ch)==-32||int(ch)==0)	//when an arrow key is pressed
 				{	
-					switch(_getch())
+					switch(_getch())	//checks which arrow key is pressed and sets the snake direction accordingly
 			    	{
 			        	case 72:
 			        	//cout<<"UP";
@@ -149,7 +147,7 @@ class Window
 			    	}
 				}
 			}
-			else
+			else	//If no key is pressed keep snake moving in its initial direction
 			{
 				switch(snk_direction)
 		    	{
@@ -171,12 +169,14 @@ class Window
 		    	}
 				
 			}
-			if(snk->getHeadX()==0||snk->getHeadX()==length-1||snk->getHeadY()==0||snk->getHeadY()==hieght-1)
+			
+			if(snk->getHeadX()==0||snk->getHeadX()==length-1||snk->getHeadY()==0||snk->getHeadY()==hieght-1)	//When snake hits boundary
 			{
 				break;
 			}
-			if(snk->getHeadX()==fd->foodx && snk->getHeadY()==fd->foody)
+			if(snk->getHeadX()==fd->foodx && snk->getHeadY()==fd->foody)	//When snake eats food
 			{
+				score+=10;
 				switch(snk_direction)
 		    	{
 		        	case 72:
@@ -200,15 +200,15 @@ class Window
 		        	break;
 		    	}
 			}
-			draw();
-			Sleep(10);
+			draw();		//To display snake on screen
+			Sleep(2);	//To give a delay of 2msec
 		}
 	}
 	
-	void draw()
+	void draw()		//To draw the actual snake game on screen
 	{
-		system("cls");
-		arr=snk->showSnake();
+		system("cls");	//To clear the screen
+		arr=snk->showSnake();	//To get the updated snake coordinates
 		bool flag;
 		for(int i=0;i<length;i++)
 		{
@@ -217,7 +217,7 @@ class Window
 				flag=0;
 				if(i==0||i==length-1||j==0||j==hieght-1)
 				{
-					cout<<"#";
+					cout<<"#";	//Boundary
 					flag=1;
 				}
 				else
@@ -227,34 +227,55 @@ class Window
 						if(i==arr[2*k]&&j==arr[2*k+1])
 						{
 							if(k==0)
-								cout<<"@";
+								cout<<"@";	//Snake head
 							else
-								cout<<"0";
+								cout<<"0";	//Snake body
 							flag=1;
 						}
 					}
 				}
 				if(i==fd->foodx&&j==fd->foody)
 				{
-					cout<<"=";
+					cout<<"+";	//snake food
 					flag=1;
 				}
 				if(flag==0)
 				{
-					cout<<" ";
+					cout<<" ";	//Empty space
 				}
 			}
 			cout<<endl;
 		}
-//		For Debugging:
-//		for(int k=0;k<snk->getLength();k++)
-//			cout<<arr[2*k]<<" "<<arr[2*k+1]<<" ";
+		cout<<"Score: "<<score;
 	}
 };
 
 int main()
 {
-	Window win(30,50,15,25);
-	win.rungame();
+	char ch,ch2;
+	cout<<"\n\tWelcome To The Snake Game!\n"<<endl;
+	cout<<"Instructions:"<<endl;
+	cout<<"1. Fullscreen the console window for better experience."<<endl;
+	cout<<"2. Use arrow keys to move the snake."<<endl;
+	cout<<"3. Press Esc to exit the game."<<endl;
+	cout<<"4. Press Space to start the game."<<endl;
+	
+	while(true)
+	{
+		if(kbhit())
+		{
+			ch = _getch();
+			if(int(ch)==32)		//32-->ascii for Space
+			{
+				Window win(40,80,20,40);
+				win.rungame();
+				cout<<"\nGame Over!"<<endl<<"Press Space Key to play again..."<<endl<<"Press Esc Key to exit...";
+			}
+			else if(int(ch)==27)
+			{
+				break;
+			}
+		}
+	}
 	return 0;
 }
